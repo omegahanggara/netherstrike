db301681fb868942e94a00d92b8a42bc5c1c06d4
Krisan Alfa Timur <krisan47@gmail.com>
2013-06-07 17:28:00 +0700

    Better logging and functionality

--- krisanalfa/git_to_twitter.sh
+++ krisanalfa/git_to_twitter.sh
@@ -1,13 +1,35 @@
 #!/usr/bin/env bash
 # Author: Krisan Alfa Timur
 
+hashtag=;
+repo=;
+github_url=;
+comit_message=;
+rev_number=;
+url=;
+tweets=;
+
+postToTwitter() {
+    echo "$tweets" >> ~/.githubTweets.lst;                                 # logging
+    cat ~/.githubTweets.lst | sed '/^$/d' | uniq > /tmp/githubTweets.lst   # removing duplicate entry
+    rm ~/.githubTweets.lst
+    cat /tmp/githubTweets.lst | uniq > ~/.githubTweets.lst
+    exist=$(cat ~/.githubTweets.lst | grep "$tweets");
+    if [[ $exist == "" ]]; then
+        twidge update "$tweets";
+        echo "Posting to twitter."
+    else
+        echo "You have tweet that tweet!"
+    fi
+}
+
 command -v twidge > /dev/null
 if [[ $? -eq 0 ]]; then
     if [[ -d .git ]]; then
         read_git_hub=$(cat .git/config | grep github);
         if [[ $read_git_hub == "" ]]; then
             echo "This commit will not posted on Twitter";
-            # git push origin master
+            git push origin master
         else
             git log --pretty=oneline > /dev/null 2>&1
             if [[ $? -eq 0 ]]; then
@@ -18,23 +40,14 @@ if [[ $? -eq 0 ]]; then
                 rev_number=$(git log --pretty=oneline | head -n 1 | awk -F ' ' '{print $1}');
                 url="$github_url/$repo/commit/$rev_number";
                 tweets="Update on $hashtag. $comit_message $url #$hashtag";
-                # echo "Will be posted on Twitter"
-                # echo "GitHub URL: $url";
-                # echo "Commit message = $comit_message";
-                # echo "Hashtag: $hashtag";
-                # echo "Your tweets: $tweets";
-                echo -e "$tweets\n" >> ~/.githubTweets.lst;                  # logging
-                # cat ~/.githubTweets.lst | uniq > ~/.githubTweets.lst   # removing duplicate entry
-                # exist=$(cat ~/.githubTweets.lst | grep "$tweets");
-                # echo $exist;
-                # if [[ $exist == "" ]]; then
-                #     twidge update "$tweets";
-                #     echo "Posting to twitter."
-                # else
-                #     echo "You have tweet that tweet!"
-                # fi
-                # echo "Pushing codes to $github_url/$repo"
-                # git push origin master
+                echo "Will be posted on Twitter"
+                echo "GitHub URL: $url";
+                echo "Commit message = $comit_message";
+                echo "Hashtag: $hashtag";
+                echo "Your tweets: $tweets";
+                postToTwitter;
+                echo "Pushing codes to $github_url/$repo"
+                git push origin master
             else
                 echo "You just initialize this repo and there is no commit for it."
             fi
